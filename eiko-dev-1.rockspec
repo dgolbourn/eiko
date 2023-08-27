818c338e089d25bb6d77ef32eb3b45cc8ebca2df
Diane Golbourn <golbourn@gmail.com>
2023-08-27 20:07:22 +0000

    luarocks test

--- eiko-dev-1.rockspec
+++ eiko-dev-1.rockspec
@@ -17,5 +17,5 @@ build = {
 }
 test = {
    type = "command",
-   command = "./test/test.sh"
+   command = "./test/test.sh ./test --verbose"
 }
--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+TEST_DIR=$1
+shift
+export LUATEST_OPTIONS=$*
+echo "Searching for test fixtures in $TEST_DIR"
+echo "Running luatest with options: $LUATEST_OPTIONS"
 echo
-find $(dirname "$0") -type f -name "test_*.lua" | xargs -l bash -c 'luajit "$@" --verbose && echo' dummy
+run_tests(){
+    echo "Test file: $1"
+    luajit $1 $LUATEST_OPTIONS
+    echo
+    return 0
+}
+export -f run_tests
+find $TEST_DIR -type f -name "test_*.lua" | xargs -l bash -c 'run_tests "$@"' _
