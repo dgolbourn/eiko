74f0cafcbaa901f4de0e8a90bd61354c742375f4
Diane Golbourn <golbourn@gmail.com>
2023-08-27 14:05:37 +0100

    adding dev container

---
+++ .devcontainer/lua/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install build-essential libreadline-dev unzip libssl-dev rlwrap
+
+git clone https://luajit.org/git/luajit.git
+cd luajit
+make
+sudo make install
+sudo ln -sf luajit /usr/local/bin/luajit
+cd ../
+
+git clone https://github.com/luarocks/luarocks.git
+cd luarocks
+./configure --force-config --with-lua-interpreter=luajit --lua-version=5.1
+make
+sudo make install
+cd ../
