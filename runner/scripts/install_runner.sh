ba5082ad64e9ec3f41ab1765c0370da3c4f100aa
Diane Golbourn <golbourn@gmail.com>
2023-08-28 16:32:52 +0100

    runner

--- runner/scripts/install_runner.sh
+++ runner/scripts/install_runner.sh
@@ -2,7 +2,7 @@
 sudo apt update -y
 sudo apt install -y curl
 
-mkdir actions-runner 
+mkdir actions-runner
 cd actions-runner
 curl -o actions-runner-linux-x64-2.308.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.308.0/actions-runner-linux-x64-2.308.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.308.0.tar.gz
