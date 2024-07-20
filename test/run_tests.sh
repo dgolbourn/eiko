e9abb2fee0923b799e3a093033ffa452879d10c9
Diane Marigold <golbourn@gmail.com>
2024-07-20 19:47:57 +0000

    :(

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -10,6 +10,7 @@ echo "Luatest options: $LUATEST_OPTIONS"
 echo
 
 mkdir reports
+
 run_tests(){
     echo "Test file: $1"
     luajit $1 $LUATEST_OPTIONS
@@ -19,5 +20,6 @@ run_tests(){
     echo
     return $STATUS
 }
+
 export -f run_tests
 find $TEST_DIR -type f -name "test_*.lua" | xargs -l bash -c 'run_tests "$@"' _
