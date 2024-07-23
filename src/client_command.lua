c88791f0cbe9379134dfa2ccc2f0e2bbc1e513df
Diane Marigold <golbourn@gmail.com>
2024-07-23 19:54:47 +0000

    client_command tcp server skeleton

--- src/client_command.lua
+++ src/client_command.lua
@@ -1,7 +1,65 @@
-local function callback(loop, timer_event)
-    print("client command")
+local socket = require("socket")
+local ev = require("ev")
+
+local server_state = nil
+
+local client_states = {}
+
+local function on_client_io_event(peername, loop, io, revents)
+    print("on_client_io_event " .. peername)
+    client_state = client_states[peername]
+    local data, err, partial = client_state.client:receive('*a', client_state.buffer)
+    client_state.buffer = partial
+    if data then
+    elseif err == "timeout" then
+    elseif err == "closed" then
+        client_state.io_watcher:stop(ev.Loop.default)
+        client_states[peername] = nil
+    end
+end
+
+local function on_server_io_event(loop, io, revents)
+    print("on_server_io_event")
+    local client = server_state.server:accept()
+    client:settimeout(0)
+    local client_state = {}
+    local peername = client:getpeername()
+    local io_event = function(loop, io, revents)
+        on_client_io_event(peername, loop, io, revents)
+    end
+    local io_watcher = ev.IO.new(io_event, client:getfd(), ev.READ)
+    io_watcher:start(ev.Loop.default)
+    client_state.client = client
+    client_state.io_watcher = io_watcher
+    client_states[client:getpeername()] = client_state
+end
+
+local function start(host, port)
+    local server = socket.tcp()
+    server:bind(host, port)
+    local max_client = 16
+    server:listen(max_clients)
+    server:settimeout(0)
+    local _server_state = {}
+    _server_state.server = server
+    local io_watcher = ev.IO.new(on_server_io_event, server:getfd(), ev.READ)
+    io_watcher:start(ev.Loop.default)
+    _server_state.io_watcher = io_watcher
+    server_state = _server_state
+end
+
+local function stop()
+    for k, client_state in client_states do
+        client_state.io_watcher:stop(ev.Loop.default)
+        client_state.client:close()
+    end
+    client_states = {}
+    server_state.io_watcher:stop(ev.Loop.default)
+    server_state.server:close()
+    server_state = nil
 end
 
 return {
-    callback = callback
+    start = start,
+    stop = stop
 }
--- src/eiko.lua
+++ src/eiko.lua
@@ -3,9 +3,7 @@ local se = require "eiko.server_event"
 local cc = require "eiko.client_command"
 local gl = require "eiko.game_loop"
 
-local client_command_period = 1./10
-local client_command_timer = ev.Timer.new(cc.callback, client_command_period, client_command_period)
-client_command_timer:start(ev.Loop.default)
+cc.start("localhost", 21098)
 
 local server_event_period = 1./20
 local server_event_timer = ev.Timer.new(se.callback, server_event_period, server_event_period)
