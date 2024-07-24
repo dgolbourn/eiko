04f5e5f1d93d2bb0158ae043159fba5eef0f62e5
Diane Marigold <golbourn@gmail.com>
2024-07-24 20:24:04 +0000

    delta compress store counters, added non delta compress enc and dec

--- src/encdec.lua
+++ src/encdec.lua
@@ -4,24 +4,45 @@ local dmp = require "diff_match_patch"
 local snappy = require "resty.snappy"
 local sodium = require "sodium"
 
-local function delta_compress_encode(new, previous, key)
+local function delta_compress_encode(new, new_counter, previous, previous_counter, key)
     local diffs = dmp.diff_main(previous, new)
     local patches = dmp.patch_make(previous, diffs)
     local text = dmp.patch_toText(patches)
-    local compressed = snappy.compress(text)
+    local counters = string.format("%016X", new_counter) .. string.format("%016X", previous_counter)
+    local counterstext = counters .. text
+    local compressed = snappy.compress(counters .. text)
     local nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES)
     local secret = sodium.crypto_secretbox_easy(compressed, nonce, key)
     return nonce .. secret
 end
 
-local function delta_compress_decode(noncesecret, previous, key)
+local function delta_compress_decode(noncesecret, previous_map, key)
     local nonce = string.sub(noncesecret, 1, sodium.crypto_secretbox_NONCEBYTES)
     local secret = string.sub(noncesecret, sodium.crypto_secretbox_NONCEBYTES + 1, -1)
     local compressed = sodium.crypto_secretbox_open_easy(secret, nonce, key)
-    local text = snappy.uncompress(compressed)
+    local counterstext = snappy.uncompress(compressed)
+    local new_counter = tonumber(string.sub(counterstext, 1, 16), 16)
+    local previous_counter = tonumber(string.sub(counterstext, 17, 32), 16)
+    local previous = previous_map[previous_counter]
+    local text = string.sub(counterstext, 33, -1)
     local patches = dmp.patch_fromText(text)
     local new = dmp.patch_apply(patches, previous)
-    return new
+    return new, new_counter
+end
+
+local function encode(message, key)
+    local compressed = snappy.compress(message)
+    local nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES)
+    local secret = sodium.crypto_secretbox_easy(compressed, nonce, key)
+    return nonce .. secret
+end
+
+local function decode(noncesecret, key)
+    local nonce = string.sub(noncesecret, 1, sodium.crypto_secretbox_NONCEBYTES)
+    local secret = string.sub(noncesecret, sodium.crypto_secretbox_NONCEBYTES + 1, -1)
+    local compressed = sodium.crypto_secretbox_open_easy(secret, nonce, key)
+    local message = snappy.uncompress(compressed)
+    return message
 end
 
 local function authenticate(message, key)
@@ -30,13 +51,15 @@ end
 
 local function verify(messagetag, key)
     local message = string.sub(messagetag, 1, -sodium.crypto_auth_BYTES -1)
-    local tag = string.sub(messagetag, -sodium.crypto_auth_BYTES, -1)    
+    local tag = string.sub(messagetag, -sodium.crypto_auth_BYTES, -1)
     return sodium.crypto_auth_verify(tag, message, key)
 end
 
 return {
-    encode = encode, 
+    encode = encode,
     decode = decode,
+    delta_compress_encode = delta_compress_encode,
+    delta_compress_decode = delta_compress_decode,
     authenticate = authenticate,
     verify = verify
 }
--- test/test_encdec.lua
+++ test/test_encdec.lua
@@ -2,7 +2,20 @@ local lu = require "luaunit"
 
 Test = {}
 
-function Test:test_encdec()
+function Test:test_enc_dec()
+    local encdec = require "eiko.encdec"
+    local cjson = require "cjson"
+    local sodium = require "sodium"
+    local payload = {a={"blah"}}
+    local key = sodium.crypto_secretbox_keygen()
+    local new = cjson.encode(payload)
+    local noncesecret = encdec.encode(new, key)
+    local decoded_new = encdec.decode(noncesecret, key)
+    local decoded_payload = cjson.decode(decoded_new)
+    lu.assertEquals(payload, decoded_payload)
+end
+
+function Test:test_delta_compress_enc_dec()
     local encdec = require "eiko.encdec"
     local cjson = require "cjson"
     local sodium = require "sodium"
@@ -10,13 +23,18 @@ function Test:test_encdec()
     local previous = [[{"a":["foo"]}]]
     local key = sodium.crypto_secretbox_keygen()
     local new = cjson.encode(payload)
-    local noncesecret = encdec.delta_compress_encode(new, previous, key)
-    local decoded_new = encdec.delta_compress_decode(noncesecret, previous, key)
+    local new_counter = 1
+    local previous_counter = 0
+    local noncesecret = encdec.delta_compress_encode(new, new_counter, previous, previous_counter, key)
+    local previous_map = {}
+    previous_map[previous_counter] = previous
+    local decoded_new, decoded_new_counter = encdec.delta_compress_decode(noncesecret, previous_map, key)
     local decoded_payload = cjson.decode(decoded_new)
+    lu.assertEquals(new_counter, decoded_new_counter)
     lu.assertEquals(payload, decoded_payload)
 end
 
-function Test:test_authverify()
+function Test:test_auth_verify()
     local encdec = require "eiko.encdec"
     local cjson = require "cjson"
     local sodium = require "sodium"
