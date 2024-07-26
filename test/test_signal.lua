c54819ba49153253d9f74d3417e69b98fd736ac6
Diane Marigold <golbourn@gmail.com>
2024-07-26 00:32:53 +0000

    signal

--- test/test_signal.lua
+++ test/test_signal.lua
@@ -5,6 +5,7 @@ Test = {}
 function Test:test_signal()
     local signal = require "signal"
     print(signal.SIGUSR1)
+    signal.signal(signal.SIGUSR1, function() end)
     signal.raise(signal.SIGUSR1)
 end
 
