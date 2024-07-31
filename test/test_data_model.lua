bf360af41920d7de7f0b474538d1a808d637a3f2
Diane Marigold <golbourn@gmail.com>
2024-07-31 19:00:45 +0000

    oops

--- test/test_data_model.lua
+++ test/test_data_model.lua
@@ -4,8 +4,8 @@ Test = {}
 
 function Test:test_command()
     local data_model = require "eiko.data_model"
-    local message1 = assert(data_model.command1.decode('{"_kind":"command1","foo":"foo"}'))
-    local expected_message1 = {_kind="command1",foo="foo"}
+    local message1 = assert(data_model.client_ack_action.decode('{"_kind":"client_ack_action"}'))
+    local expected_message1 = {_kind="client_ack_action"}
     lu.assertEquals(message1, expected_message1)
 end
 
