9233285198d2896f3804da61b310a7c11cefbfc2
Diane Marigold <golbourn@gmail.com>
2024-08-12 22:02:18 +0000

    tweak codec

--- src/codec.lua
+++ src/codec.lua
@@ -7,11 +7,11 @@ local function delta_compress_encode(new, new_epoch, previous, previous_epoch, k
     local diffs = dmp.diff_main(previous, new)
     local patches = dmp.patch_make(previous, diffs)
     local text = dmp.patch_toText(patches)
-    local compressed = snappy.compress(text)
     local epochs = string.format("%016X", new_epoch) .. string.format("%016X", previous_epoch)
-    local epochscompressed = epochs .. compressed
+    local epochstext = epochs .. text
+    local compressed = snappy.compress(epochstext)
     local nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES)
-    local secret = sodium.crypto_secretbox_easy(epochscompressed, nonce, key)
+    local secret = sodium.crypto_secretbox_easy(compressed, nonce, key)
     local noncesecret = nonce .. secret
     return noncesecret
 end
@@ -19,23 +19,23 @@ end
 local function delta_compress_decode(noncesecret, previous_map, key)
     local nonce = string.sub(noncesecret, 1, sodium.crypto_secretbox_NONCEBYTES)
     local secret = string.sub(noncesecret, sodium.crypto_secretbox_NONCEBYTES + 1, -1)
-    local epochscompressed = sodium.crypto_secretbox_open_easy(secret, nonce, key)
-    local new_epoch = tonumber(string.sub(epochscompressed, 1, 16), 16)
-    local previous_epoch = tonumber(string.sub(epochscompressed, 17, 32), 16)
-    local previous = previous_map[previous_epoch]
-    local compressed = string.sub(epochscompressed, 33, -1)
-    local text = snappy.uncompress(compressed)
+    local compressed = sodium.crypto_secretbox_open_easy(secret, nonce, key)
+    local epochstext = snappy.uncompress(compressed)
+    local text = string.sub(epochstext, 33, -1)
+    local new_epoch = tonumber(string.sub(epochstext, 1, 16), 16)
+    local previous_epoch = tonumber(string.sub(epochstext, 17, 32), 16)
+    local previous = previous_map[previous_epoch]        
     local patches = dmp.patch_fromText(text)
     local new = dmp.patch_apply(patches, previous)
     return new, new_epoch
 end
 
 local function encode(message, counter, epoch, key)
-    local compressed = snappy.compress(message)
     local epochs = string.format("%016X", epoch) .. string.format("%016X", counter)
-    local epochscompressed = epochs .. compressed
+    local epochsmessage = epochs .. message
+    local compressed = snappy.compress(epochsmessage)
     local nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES)
-    local secret = sodium.crypto_secretbox_easy(epochscompressed, nonce, key)
+    local secret = sodium.crypto_secretbox_easy(compressed, nonce, key)
     local noncesecret = nonce .. secret
     return noncesecret
 end
@@ -43,12 +43,12 @@ end
 local function decode(noncesecret, key)
     local nonce = string.sub(noncesecret, 1, sodium.crypto_secretbox_NONCEBYTES)
     local secret = string.sub(noncesecret, sodium.crypto_secretbox_NONCEBYTES + 1, -1)
-    local epochscompressed, err = sodium.crypto_secretbox_open_easy(secret, nonce, key)
-    if epochscompressed then
-        local epoch = tonumber(string.sub(epochscompressed, 1, 16), 16)
-        local counter = tonumber(string.sub(epochscompressed, 17, 32), 16)
-        local compressed = string.sub(epochscompressed, 33, -1)
-        local message = snappy.uncompress(compressed)
+    local compressed, err = sodium.crypto_secretbox_open_easy(secret, nonce, key)
+    if compressed then
+        local epochsmessage = snappy.uncompress(compressed)
+        local epoch = tonumber(string.sub(epochsmessage, 1, 16), 16)
+        local counter = tonumber(string.sub(epochsmessage, 17, 32), 16)
+        local message = string.sub(epochsmessage, 33, -1)
         return message, counter, epoch
     else
         return nil, err
