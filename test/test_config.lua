5ee520bde9400b28116bd37d92bd3e524c8a37f0
Diane Marigold <golbourn@gmail.com>
2024-08-16 21:38:00 +0000

    remove libyaml dependency

--- test/test_config.lua
+++ test/test_config.lua
@@ -4,12 +4,12 @@ Test = {}
 
 function Test:test_config()
     local config = require "eiko.config"
-    local lyaml = require "lyaml"
-    print(lyaml.dump{config.load("res/server.yaml")})
-    print(lyaml.dump{config.load("res/authenticator.yaml")})
-    print(lyaml.dump{config.load("res/client1.yaml")})
-    print(lyaml.dump{config.load("res/client2.yaml")})
-    print(lyaml.dump{config.load("res/client3.yaml")})
+    local yaml = require "yaml"
+    yaml.dump(config.load("res/server.yaml"))
+    yaml.dump(config.load("res/authenticator.yaml"))
+    yaml.dump(config.load("res/client1.yaml"))
+    yaml.dump(config.load("res/client2.yaml"))
+    yaml.dump(config.load("res/client3.yaml"))
 end
 
 os.exit(lu.LuaUnit.run())
